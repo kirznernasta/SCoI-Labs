9c3eeda8a38debaa33d92d4c89ea2b18efea1132
kirznernasta <kirznernasta@gmail.com>
2023-02-19 22:06:13 +0300

    Add Dockerfile and requirements.txt, modify main.py

--- Lab1/main.py
+++ Lab1/main.py
@@ -3,6 +3,6 @@ from hello_world import say_hello_world
 from operation import calculate_operation
 
 if __name__ == '__main__':
-    # say_hello_world()
-    # calculate_operation(1, 0, 'div')
-    print(generate_list_of_even_numbers())
+    say_hello_world()
+    print('calculate_operation function with arguments 1, 2, "add" returned:', calculate_operation(1, 2, 'add'))
+    print('list of even numbers with maximum length 15:', generate_list_of_even_numbers())
